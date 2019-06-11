9fb7735952692837cbc6a48073cf09e76f9932c0
张贵新 <hobbin@192.168.0.102>
2019-06-11 14:56:09 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
+const path = require('path');
 
-module.exports = {
-  entry: './index.js',
-  output: {
-    path: __dirname + '/build', //默认配置：/dist
-    filename: 'bundle.js'       //默认配置：main.js
-  },
-  mode: 'production',
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        plugins: ['transform-runtime'],
-        presets: ['es2015', 'react', 'stage-2']
-      }
-    }, {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader'
-    }]
-  }
-};
+ module.exports = {
+	 mode: 'development',
+	 	entry: './index.js',
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'output.bundle.js',
+		},
+	 module: {
+		 rules: [
+			 {
+				 test: /\.(m?js|jsx)$/,
+				 exclude: /(node_modules|bower_components)/,
+				 use: {
+					 loader: 'babel-loader',
+					 options: {
+						 presets: ['@babel/preset-env'],
+						 plugins: ['@babel/plugin-transform-react-jsx']
+					 }
+				 }
+			 }
+		 ]
+	 }
+ }
