a36987df38d995e835fd2c485e917647278c54cc
张贵新 <hobbin@192.168.0.101>
2019-06-12 16:00:32 +0800

    start

---
+++ index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import styles from './index.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>Hello World!!aggggdsfss030</div>
+        )
+    }
+}
+
+ReactDom.render(<App/>, document.getElementById('container'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,38 @@
 const path = require('path');
+const webpack = require('webpack');
 
- module.exports = {
-	 mode: 'development',
-	 	entry: './index.js',
-		output: {
-			path: path.resolve(__dirname, 'dist'),
-			filename: 'output.bundle.js',
-		},
-	 module: {
-		 rules: [
-			 {
-				 test: /\.(m?js|jsx)$/,
-				 exclude: /(node_modules|bower_components)/,
-				 use: {
-					 loader: 'babel-loader',
-					 options: {
-						 presets: ['@babel/preset-env'],
-						 plugins: ['@babel/plugin-transform-react-jsx']
-					 }
-				 }
-			 }
-		 ]
-	 }
- }
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+		devServer: {
+			inline: true,
+			port: 8000,
+			host: "0.0.0.0",
+			// compress: true,
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/react'],
+                    }
+                },
+                exclude: /node_modules/
+            }, 
+						{
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+						}
+        ]
+    },
+		plugins: [
+     	// new webpack.HotModuleReplacementPlugin(), //热加载插件
+  	],
+};
